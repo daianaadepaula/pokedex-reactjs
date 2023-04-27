11e072bec69e4c7774984bfc20556817703fb22a
Daiana de Paula <daianaadepaula1@gmail.com>
2023-04-27 09:46:26 -0300

    :sparkles: feat: add table

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -11,19 +11,16 @@ export const Profile = ({ pokemonData }) => {
       <Navbar hideSearch />
       <Container maxWidth="md">
         <Paper elevation={3}>
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignContent="center"
-            p={5}
-          >
-            <Typography variant="h5">{name}</Typography>
-            <Box
-              component="img"
-              src={sprites.other.dream_world.front_default}
-              width="25%"
-            />
-            <PokemonTable />
+          <Box display="flex" flexDirection="column" p={5}>
+            <Box display="flex" justifyContent="space-between" m={5}>
+              <Typography variant="h3">{name}</Typography>
+              <Box
+                component="img"
+                src={sprites.other.dream_world.front_default}
+                width="25%"
+              />
+            </Box>
+            <PokemonTable pokemonData={pokemonData} />
           </Box>
         </Paper>
       </Container>
